39b565b214c698f3b891e3dcce64a209e212279a
jduffield65 <jduffield65@gmail.com>
2022-11-17 13:33:53 +0000

    First look at analysing MSE distribution with latitude

--- isca_tools/__init__.py
+++ isca_tools/__init__.py
@@ -2,3 +2,4 @@ from .run import run_job, run_experiment
 from .utils import load_dataset, load_namelist
 from .time_series import create_time_series_file
 from .land import write_land
+from . import plot
