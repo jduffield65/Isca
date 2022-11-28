97f5394f83fe0398c121af17967813ed0e4bde0a
jduffield65 <jduffield65@gmail.com>
2022-11-28 13:20:11 +0000

    Use odeint to solve ode equation when computing moist lapse rate

--- isca_tools/utils/moist_physics.py
+++ isca_tools/utils/moist_physics.py
@@ -1,6 +1,8 @@
 import numpy as np
 from typing import Union
 from .constants import lapse_dry, L_v, R, R_v, epsilon, c_p, temp_kelvin_to_celsius, kappa, g
+from scipy.integrate import odeint
+import numpy_indexed
 
 
 def lcl_temp(temp_surf: np.ndarray, rh_surf: np.ndarray) -> np.ndarray:
@@ -153,34 +155,76 @@ def moist_profile(temp_start:float, p_start: float, p_levels: np.ndarray) -> np.
         `float [n_p_levels]`.</br>
         Temperature at each pressure level indicated by `p_levels`.
     """
-    p_done = np.asarray([p_start]).reshape(-1, 1)
-    p_todo = p_levels
-    temp_levels = np.zeros_like(p_levels)
-    # For each pressure level, compute lapse rate based on pressure level closest to it and then update
-    # temperature of that level assuming lapse rate stays constant between the levels.
-    while len(p_todo) > 0:
-        # Find the two pressure levels with the minimum distance between them such that on is in the set p_done
-        # and one is in the set p_todo.
-        # Want two closest as assuming lapse rate is constant between the levels
-        diff = np.abs(p_done - p_todo)
-        done_ind, todo_ind = np.argwhere(diff == np.min(diff))[0]
-        if done_ind == 0:
-            # First index is the inputted start values
-            p_ref = p_start
-            temp_ref = temp_start
+
+    # Solve for all pressure levels with lower value than starting pressure
+    p_lower = p_levels[p_levels <= p_start][::-1]
+    if len(p_lower) > 0:
+        if p_start not in p_lower:
+            added_p_start = True
+            p_lower = np.concatenate(([p_start], p_lower))
         else:
-            p_ind = np.where(p_levels == p_done[done_ind])[0]
-            p_ref = p_levels[p_ind]
-            temp_ref = temp_levels[p_ind]
+            added_p_start = False
+        temp_lower = odeint(lapse_moist, temp_start, p_lower, args=(True,)).flatten()
+        if added_p_start:
+            # If had to add starting pressure, remove it so only have temperatures corresponding to pressures in
+            # p_levels.
+            temp_lower = temp_lower[1:]
+            p_lower = p_lower[1:]
+    else:
+        temp_lower = np.array([])
 
-        # Compute lapse rate based on temperature and pressure of level in the done set.
-        dT_dp = lapse_moist(temp_ref, p_ref, True)
-        temp_level_ind = np.where(p_levels == p_todo[todo_ind])[0]
+    # Solve for all pressure levels with higher value than starting pressure
+    p_higher = p_levels[p_levels > p_start]
+    if len(p_higher) > 0:
+        if p_start not in p_higher:
+            added_p_start = True
+            p_higher = np.concatenate(([p_start], p_higher))
+        else:
+            added_p_start = False
+        temp_higher = odeint(lapse_moist, temp_start, p_higher, args=(True,)).flatten()
+        if added_p_start:
+            # If had to add starting pressure, remove it so only have temperatures corresponding to pressures in
+            # p_levels.
+            temp_higher = temp_higher[1:]
+            p_higher = p_higher[1:]
+    else:
+        temp_higher = np.array([])
 
-        # Update temperature at level considering - assume constant lapse rate between levels.
-        temp_levels[temp_level_ind] = temp_ref + (p_levels[temp_level_ind]-p_ref) * dT_dp
+    # Combine all temperature values found
+    p_descend = np.concatenate((p_higher, p_lower))
+    temp_descend = np.concatenate((temp_higher, temp_lower))
+    # Rearrange temperature values so match how p_levels ordered in input.
+    temp_final = temp_descend[numpy_indexed.indices(p_descend, p_levels)].flatten()
 
-        # Transfer pressure level from p_todo to p_done for next iteration
-        p_done = np.append(p_done, np.asarray(p_todo[todo_ind]).reshape(-1, 1), axis=0)
-        p_todo = np.delete(p_todo, todo_ind)
-    return temp_levels
+    # Old manual way of doing it
+    # p_done = np.asarray([p_start]).reshape(-1, 1)
+    # p_todo = p_levels
+    # temp_levels = np.zeros_like(p_levels)
+    # # For each pressure level, compute lapse rate based on pressure level closest to it and then update
+    # # temperature of that level assuming lapse rate stays constant between the levels.
+    # while len(p_todo) > 0:
+    #     # Find the two pressure levels with the minimum distance between them such that on is in the set p_done
+    #     # and one is in the set p_todo.
+    #     # Want two closest as assuming lapse rate is constant between the levels
+    #     diff = np.abs(p_done - p_todo)
+    #     done_ind, todo_ind = np.argwhere(diff == np.min(diff))[0]
+    #     if done_ind == 0:
+    #         # First index is the inputted start values
+    #         p_ref = p_start
+    #         temp_ref = temp_start
+    #     else:
+    #         p_ind = np.where(p_levels == p_done[done_ind])[0]
+    #         p_ref = p_levels[p_ind]
+    #         temp_ref = temp_levels[p_ind]
+    #
+    #     # Compute lapse rate based on temperature and pressure of level in the done set.
+    #     dT_dp = lapse_moist(temp_ref, p_ref, True)
+    #     temp_level_ind = np.where(p_levels == p_todo[todo_ind])[0]
+    #
+    #     # Update temperature at level considering - assume constant lapse rate between levels.
+    #     temp_levels[temp_level_ind] = temp_ref + (p_levels[temp_level_ind]-p_ref) * dT_dp
+    #
+    #     # Transfer pressure level from p_todo to p_done for next iteration
+    #     p_done = np.append(p_done, np.asarray(p_todo[todo_ind]).reshape(-1, 1), axis=0)
+    #     p_todo = np.delete(p_todo, todo_ind)
+    return temp_final
