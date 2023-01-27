2d7bc404413874521c8038e84df4ea47ee207cda
jduffield65 <jduffield65@gmail.com>
2023-01-27 11:52:37 +0000

    Add gamma_t thresholding theory

--- isca_tools/run/run_slurm.sh
+++ isca_tools/run/run_slurm.sh
@@ -23,7 +23,7 @@ sbatch <<EOT
 # $9 - maximum allowed walltime to run job
 # ${10} - python script path to run i.e. the path of run_job_script.py
 
-# Run the run_job_script.py script for experiment and record how long it takes
+# Run the base.py script for experiment and record how long it takes
 python ${10} $7 $8 $2 $3   # run job
 
 # Move txt documents containing erros and printed info to experiment data folder
