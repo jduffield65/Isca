dba1f3f1d293149d39f6560f9a501fa59f55c652
jduffield65 <jduffield65@gmail.com>
2023-12-12 12:52:05 +0000

    Add shaw_2023 notebook

--- isca_tools/utils/constants.py
+++ isca_tools/utils/constants.py
@@ -1,4 +1,5 @@
 from scipy.constants import Stefan_Boltzmann
+import numpy as np
 
 ### The constants below are from Holton 2004 Appendix A
 
@@ -44,3 +45,6 @@ temp_kelvin_to_celsius = 273.15
 
 # Radius of the earth in meters
 radius_earth = 6.3781e6
+
+# Rotation of earth in radians/s. I.e. the Omega factor in the coriolis parameter: 2 Omega sin(theta)
+rot_earth = 2*np.pi/(24*60**2)
