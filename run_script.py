74d52d150f2a1fa9b31f0023ca9a2bc52e456207
jduffield65 <jduffield65@gmail.com>
2025-02-04 07:34:40 +0000

    Update run_script.py

--- run_script.py
+++ run_script.py
@@ -1,10 +1,33 @@
-from isca_tools import run_experiment
+from isca_tools.run import run_experiment
 import os
+# import os
+# from functools import wraps
+#
+# import sh
+#
+# mkdir = sh.mkdir.bake('-p')
+# cd = sh.cd
+# git = sh.git.bake('--no-pager')
+#
+# P = os.path.join
+# codedir_git = git.bake('-C', os.environ['GFDL_BASE'])
+# git_test = codedir_git.log('-1', '--format="%H"').stdout
 
 # When calling the script with no arguments, it just runs the experiment.
 jobs_dir = os.path.join(os.environ['HOME'], 'Isca', 'jobs')  # all jobs saved here - CHANGE FOR EACH EXPERIMENT
-# exp_dir = os.path.join(jobs_dir, 'tau_sweep/aquaplanet/k=6')  # specific experiment - CHANGE FOR EACH EXPERIMENT
-exp_dir = os.path.join(jobs_dir, 'tau_sweep/land/meridional_band/width=30deg/bucket_evap/evap=0_1/k=1_5')
+# exp_dir = os.path.join(jobs_dir, 'tau_sweep/aquaplanet/depth=1/no_conv/k=1')  # specific experiment - CHANGE FOR EACH EXPERIMENT
+# exp_dir = os.path.join(jobs_dir, 'tau_sweep/land/meridional_band/depth=1/bucket_evap/ras_conv/k=1_5')  # specific experiment - CHANGE FOR EACH EXPERIMENT
+# exp_dir = os.path.join(jobs_dir, 'tau_sweep/land/meridional_band/evap/evap=0/k=0_6')
+# exp_dir = os.path.join(jobs_dir, 'aquaplanet/vary_depth/depth=25')
+# exp_dir = os.path.join(jobs_dir, 'aquaplanet/vary_obliquity/5')
+# exp_dir = os.path.join(jobs_dir, 'aquaplanet/vary_depth/depth=1/k=0_3')
+# exp_dir = os.path.join(jobs_dir, 'play/temp_tendency/k=3_5')
+# exp_dir = os.path.join(jobs_dir, 'tau_sweep/land/meridional_band/evap/evap=0/k=1_5')
+# exp_dir = os.path.join(jobs_dir, 'play/bucket/depth=5/k=1_5')
+# exp_dir = os.path.join(jobs_dir, 'rossbypalooza/depth=20/lat_10/rough')
+exp_dir = os.path.join(jobs_dir, 'aquaplanet/latent_heat_temp_only/base')
+# exp_dir = os.path.join(jobs_dir, 'rossbypalooza/depth=20/all_nh/lat_10/rot_6x')
+# exp_dir = os.path.join(jobs_dir, 'aquaplanet/latent_heat_temp_only/k=0_6')
 
 namelist_file = os.path.join(exp_dir, 'namelist.nml')
 diag_table_file = os.path.join(exp_dir, 'diag_table')
