d366b22296594e967a97d125a7b1407ac4750ee0
jduffield65 <jduffield65@gmail.com>
2025-10-21 10:38:48 +0100

    Update lcl work

--- jobs/theory_lapse/scripts/lcl.py
+++ jobs/theory_lapse/scripts/lcl.py
@@ -24,6 +24,47 @@ def get_co2_multiplier(name):
     else:
         raise ValueError(f'Not valid name = {name}')
 
+def find_lcl_empirical2(temp_env, p_env, z_env, temp_start=None, p_start=None, temp_pot_thresh=2,
+                        temp_pot_thresh_lapse=0.5, lapse_thresh=8.5):
+    # Find the lowest model layer with lapse rate less than lapse_thresh
+    # LCL is the level at which the pot temp drops by 0.5K within this layer
+    if temp_start is None:
+        temp_start = temp_env.isel(lev=-1)
+    if p_start is None:
+        p_start = p_env.isel(lev=-1)
+    temp_pot_env = potential_temp(temp_env, p_env)
+
+    # First mask is pot temp close to surface pot temperature
+    temp_pot_start = potential_temp(temp_start, p_start)
+    mask_temp = np.abs(temp_pot_env - temp_pot_start) <= temp_pot_thresh
+
+    # Second mask is lapse rate close to dry adiabat
+    # lower is so append high value at surface
+    lapse = -temp_env.diff(dim='lev', label='lower') / z_env.diff(dim='lev', label='lower') * 1000
+    lapse = lapse.reindex_like(temp_env)  # make same shape
+    lapse = lapse.fillna(lapse_thresh + 5)  # ensure final value satisfies lapse criteria
+    mask_lapse = lapse > lapse_thresh
+    mask = (mask_temp & mask_lapse)
+    lcl_ind = ((mask.where(mask, other=np.nan) * np.arange(lapse.lev.size)).min(dim='lev')).astype(int)
+
+    p_low = np.log10(p_env.isel(lev=lcl_ind))  # use log as better for interpolation - gradient is approx constant
+    p_high = np.log10(p_env.isel(lev=lcl_ind - 1))  # further from surface
+
+    # print(p_low)
+    # print(p_high)
+    # print(np.log10(p_env))
+
+    temp_pot_low = temp_pot_env.isel(lev=lcl_ind)
+    temp_pot_high = temp_pot_env.isel(lev=lcl_ind - 1)
+    gradient = (temp_pot_high - temp_pot_low) / (p_high - p_low)
+    temp_pot_target = temp_pot_low + temp_pot_thresh_lapse
+    p_target = p_low + (temp_pot_target - temp_pot_low) / gradient
+    p_target = p_target.clip(min=p_high)
+    p_lcl = 10 ** p_target
+    # print(dry_profile_temp(temp_start, p_start, p_lcl))
+    # print(lapse.isel(lev=lcl_ind-1))
+    return p_lcl, dry_profile_temp(temp_start, p_start, p_lcl)
+
 small_ds = False                 # use a small subset of data for testing
 save_processed = True
 
