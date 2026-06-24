1d25e44dda886bb0823f781323a2bfc8e9f9874b
jduffield65 <jduffield65@gmail.com>
2026-06-24 13:06:18 +0100

    Add approx version of effective heat cap

--- isca_tools/thesis/surface_energy_budget_2layer.py
+++ isca_tools/thesis/surface_energy_budget_2layer.py
@@ -3,6 +3,8 @@ import xarray as xr
 import inspect
 from typing import Union, Tuple
 
+from scipy.constants import Stefan_Boltzmann
+
 from jobs.thesis_season.thesis_figs.utils import day_seconds
 from .surface_flux_taylor_2layer import get_sensitivity_sh, get_sensitivity_lh, \
     get_sensitivity_lw_surf, get_sensitivity_lw_atm
@@ -39,8 +41,8 @@ Union[float, np.ndarray, xr.DataArray]]:
     lambda_s1 = gamma['sh']['temp_surf'] + gamma['lh']['temp_surf'] + gamma['lw_surf']['temp_surf']
     lambda_s2 = gamma['sh']['temp_surf'] + gamma['lh']['temp_surf'] + gamma['lw_atm']['temp_surf']
     lambda_a1 = -(gamma['sh']['temp_atm'] + gamma['lh']['temp_atm'] +
-                  gamma['lw_surf']['temp_rad_surf'] * (1-temp_rad_surf_coef_amp))
-    lambda_a2 = -gamma['lw_atm']['temp_rad_atm'] * (1-temp_rad_atm_coef_amp)
+                  gamma['lw_surf']['temp_rad_surf'] * (1 - temp_rad_surf_coef_amp))
+    lambda_a2 = -gamma['lw_atm']['temp_rad_atm'] * (1 - temp_rad_atm_coef_amp)
     return lambda_s1, lambda_s2, lambda_a1, lambda_a2
 
 
@@ -81,8 +83,8 @@ Union[float, np.ndarray, xr.DataArray], Union[float, np.ndarray, xr.DataArray]]:
     heat_cap_atmos = c_p * pressure_heat_cap_atmos_calc / g
     alpha_col_sphum = clausius_clapeyron_factor(temp_col_sphum, p_col_sphum)
     q_sat_col_sphum = sphum_sat(temp_col_sphum, p_col_sphum)
-    mu = L_v/c_p * alpha_col_sphum * rh_col * q_sat_col_sphum * (1 - temp_col_sphum_coef_amp)
-    f = 1/(n_year_days*day_seconds)
+    mu = L_v / c_p * alpha_col_sphum * rh_col * q_sat_col_sphum * (1 - temp_col_sphum_coef_amp)
+    f = 1 / (n_year_days * day_seconds)
     omega = 2 * np.pi * f
 
     if assume_small_temp_col_coef_phase:
@@ -93,23 +95,117 @@ Union[float, np.ndarray, xr.DataArray], Union[float, np.ndarray, xr.DataArray]]:
         temp_col_amp_param = (1 - temp_col_coef_amp) * np.cos(temp_col_coef_phase)
         temp_col_phase_param = (1 - temp_col_coef_amp) * np.sin(temp_col_coef_phase)
 
-    lambda_a2_mod = lambda_a1+omega*temp_col_phase_param*heat_cap_atmos
-    heat_cap_atmos_mod = heat_cap_atmos*(temp_col_amp_param+mu)
+    lambda_a2_mod = lambda_a1 + omega * temp_col_phase_param * heat_cap_atmos
+    heat_cap_atmos_mod = heat_cap_atmos * (temp_col_amp_param + mu)
 
     # Add lambda2 contribution, where phase can be important
     if assume_small_temp_rad_atm_phase:
         lambda_a2_mod += lambda_a2
-        heat_cap_atmos_mod -= lambda_a2 * temp_rad_atm_coef_phase/omega
+        heat_cap_atmos_mod -= lambda_a2 * temp_rad_atm_coef_phase / omega
     else:
-        lambda_a2_mod += lambda_a2*np.cos(temp_rad_atm_coef_phase)
+        lambda_a2_mod += lambda_a2 * np.cos(temp_rad_atm_coef_phase)
         heat_cap_atmos_mod -= lambda_a2 * np.sin(temp_rad_atm_coef_phase) / omega
 
     if assume_small_heat_cap_atmos:
-        lambda_s1_scaling = 1-lambda_s2*lambda_a1/lambda_s1/lambda_a2_mod
-        heat_cap_scaling = 1 + lambda_a1*lambda_s2/lambda_a2_mod**2*heat_cap_atmos_mod/heat_cap_surf
+        lambda_s1_scaling = 1 - lambda_s2 * lambda_a1 / lambda_s1 / lambda_a2_mod
+        heat_cap_scaling = 1 + lambda_a1 * lambda_s2 / lambda_a2_mod ** 2 * heat_cap_atmos_mod / heat_cap_surf
     else:
-        lambda_s1_scaling = 1 - lambda_s2*lambda_a2_mod/(lambda_a2_mod**2+omega**2*heat_cap_atmos_mod**2
-                                                         )*lambda_a1/lambda_s1
-        heat_cap_scaling = 1 + lambda_a1*lambda_s2/(lambda_a2_mod**2+omega**2*heat_cap_atmos_mod**2
-                                                         )*heat_cap_atmos_mod/heat_cap_surf
+        lambda_s1_scaling = 1 - lambda_s2 * lambda_a2_mod / (lambda_a2_mod ** 2 + omega ** 2 * heat_cap_atmos_mod ** 2
+                                                             ) * lambda_a1 / lambda_s1
+        heat_cap_scaling = 1 + lambda_a1 * lambda_s2 / (lambda_a2_mod ** 2 + omega ** 2 * heat_cap_atmos_mod ** 2
+                                                        ) * heat_cap_atmos_mod / heat_cap_surf
+    return lambda_s1, lambda_s1_scaling, heat_cap_scaling
+
+
+def get_heat_cap_lambda_eff_approx(temp_surf: Union[float, np.ndarray, xr.DataArray],
+                                   temp_atm: Union[float, np.ndarray, xr.DataArray],
+                                   temp_rad_surf: Union[float, np.ndarray, xr.DataArray],
+                                   temp_rad_atm: Union[float, np.ndarray, xr.DataArray],
+                                   rh_atm: Union[float, np.ndarray, xr.DataArray],
+                                   w_atm: Union[float, np.ndarray, xr.DataArray],
+                                   drag_coef: Union[float, np.ndarray, xr.DataArray],
+                                   p_surf: Union[float, np.ndarray, xr.DataArray],
+                                   odp_surf: Union[float, np.ndarray, xr.DataArray],
+                                   sigma_atm: float,
+                                   heat_cap_surf: Union[float, np.ndarray, xr.DataArray],
+                                   temp_col_sphum: Union[float, np.ndarray, xr.DataArray],
+                                   p_col_sphum: float,
+                                   rh_col: Union[float, np.ndarray, xr.DataArray],
+                                   pressure_heat_cap_atmos_calc: float,
+                                   evap_prefactor: float = 1,
+                                   temp_rad_surf_coef_amp: Union[float, np.ndarray, xr.DataArray] = 0,
+                                   temp_rad_atm_coef_amp: Union[float, np.ndarray, xr.DataArray] = 0,
+                                   temp_rad_atm_coef_phase: Union[float, np.ndarray, xr.DataArray] = 0,
+                                   temp_col_coef_amp: Union[float, np.ndarray, xr.DataArray] = 0,
+                                   temp_col_coef_phase: Union[float, np.ndarray, xr.DataArray] = 0,
+                                   temp_col_sphum_coef_amp: Union[float, np.ndarray, xr.DataArray] = 0,
+                                   n_year_days: int = 360,
+                                   day_seconds: int = 86400
+                                   ) -> Tuple[Union[float, np.ndarray, xr.DataArray],
+Union[float, np.ndarray, xr.DataArray], Union[float, np.ndarray, xr.DataArray]]:
+    local_vars = locals()
+    arg_names = list(inspect.signature(get_feedback_params).parameters.keys())
+    args_use = {name: local_vars[name] for name in arg_names if name in local_vars}
+    lambda_s1, lambda_s2, lambda_a1, lambda_a2 = get_feedback_params(**args_use)
+
+    heat_cap_atmos = c_p * pressure_heat_cap_atmos_calc / g
+    alpha_col_sphum = clausius_clapeyron_factor(temp_col_sphum, p_col_sphum)
+    q_sat_col_sphum = sphum_sat(temp_col_sphum, p_col_sphum)
+    mu = L_v / c_p * alpha_col_sphum * rh_col * q_sat_col_sphum * (1 - temp_col_sphum_coef_amp)
+    f = 1 / (n_year_days * day_seconds)
+    omega = 2 * np.pi * f
+    emiss_factor = 1 - np.exp(-odp_surf)
+
+    # Taylor expansion of cos and sin to 1st order
+    temp_col_amp_param = 1 - temp_col_coef_amp
+    temp_col_phase_param = (1 - temp_col_coef_amp) * temp_col_coef_phase
+
+    gamma = omega * heat_cap_atmos * temp_col_amp_param / (4 * Stefan_Boltzmann * temp_surf ** 3)
+    emiss_rad_surf = emiss_factor * (temp_rad_surf / temp_surf) ** 3 * (1 - temp_rad_surf_coef_amp)
+    emiss_rad_atm = emiss_factor * (temp_rad_atm / temp_surf) ** 3 * (1 - temp_rad_atm_coef_amp)
+    emiss_a3 = emiss_rad_surf * temp_col_coef_phase + emiss_rad_atm * (temp_col_coef_phase - temp_rad_atm_coef_phase)
+
+    # # More Complete decomp
+    # heat_cap_scaling = 1 + 2 * emiss_a3 * emiss_rad_atm * temp_rad_atm_coef_phase / (
+    #             emiss_rad_atm + emiss_rad_surf) ** 2
+    # heat_cap_scaling -= 2 * emiss_a3 * gamma / (emiss_rad_atm + emiss_rad_surf) ** 2
+    # heat_cap_scaling -= emiss_rad_atm * temp_rad_atm_coef_phase / gamma
+    # heat_cap_scaling *= emiss_factor * emiss_rad_surf / (emiss_rad_atm + emiss_rad_surf) ** 2 * \
+    #                     heat_cap_atmos * temp_col_amp_param / heat_cap_surf
+    # heat_cap_scaling += 1
+
+    # Simpler decomp
+    x = emiss_rad_surf + emiss_rad_atm
+    heat_cap_scaling = 1
+    heat_cap_scaling -= 2*temp_col_coef_phase*gamma/x
+    heat_cap_scaling -= emiss_rad_atm*temp_rad_atm_coef_phase/gamma
+    heat_cap_scaling *= emiss_factor * emiss_rad_surf / x ** 2 * \
+                        heat_cap_atmos * temp_col_amp_param / heat_cap_surf
+    heat_cap_scaling += 1
+
+
+
+    lambda_s1_scaling0 = 1 - emiss_rad_surf*(x+gamma*temp_col_coef_phase)/(gamma**2+x**2 + 2*gamma*emiss_a3)*emiss_factor
+    var = emiss_rad_surf / x ** 2 * (x + gamma * temp_col_coef_phase - (2 * gamma * emiss_a3 + gamma ** 2) / x)
+    lambda_s1_scaling = 1 - var*emiss_factor
+
+    var = emiss_rad_surf/x * (1 + ((temp_col_coef_phase*x-2*emiss_a3)*gamma-gamma**2)/x**2)
+    var = emiss_rad_surf / x * (1 - ((temp_col_coef_phase * emiss_rad_surf) * gamma + gamma ** 2) / x ** 2)
+    lambda_s1_scaling = 1 - var * emiss_factor
+
+    # lambda_s1_scaling = 1 - emiss_factor * emiss_rad_surf / (emiss_rad_surf + emiss_rad_atm)
+    # lambda_s1_scaling += emiss_factor*emiss_rad_surf*temp_col_coef_phase*gamma/(emiss_rad_atm+emiss_rad_surf)**2
+
+    # lambda_s1_scaling = 1 - emiss_factor*emiss_rad_surf/(emiss_rad_surf+emiss_rad_atm)
+    # lambda_s1_scaling -= emiss_factor*emiss_rad_surf*temp_col_coef_phase*gamma/(emiss_rad_atm+emiss_rad_surf)**2
+
+    lambda_a2_mod = lambda_a1 + omega * temp_col_phase_param * heat_cap_atmos
+    heat_cap_atmos_mod = heat_cap_atmos * (temp_col_amp_param + mu)
+
+    # Add lambda2 contribution, where phase can be important
+    lambda_a2_mod += lambda_a2
+    heat_cap_atmos_mod -= lambda_a2 * temp_rad_atm_coef_phase / omega
+
+    lambda_s1_scaling2 = 1 - lambda_s2 * lambda_a1 / lambda_s1 / lambda_a2_mod
+    heat_cap_scaling2 = 1 + lambda_a1 * lambda_s2 / lambda_a2_mod ** 2 * heat_cap_atmos_mod / heat_cap_surf
     return lambda_s1, lambda_s1_scaling, heat_cap_scaling
